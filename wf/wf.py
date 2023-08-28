885141b3294b5a8c789476a87ae325d496e922c5
Vincent K. <vincentk@hey.com>
2023-08-27 22:35:30 -0400

    Fix reading JSON on windows

--- wf/wf.py
+++ wf/wf.py
@@ -12,21 +12,21 @@ class WorldFlipperData:
         self.characters_by_name = {}
 
         # Most of the base data/attributes of any individual character. Stuff like stars, power flip type, element, etc.
-        with open(f"{data_dir}/character/character.json", "r") as f:
+        with open(f"{data_dir}/character/character.json", "r", encoding="utf-8") as f:
             character_json = json.load(f)
         # Holds a bunch of user-facing description text, most importantly for our purposes: The name.
-        with open(f"{data_dir}/character/character_text.json", "r") as f:
+        with open(f"{data_dir}/character/character_text.json", "r", encoding="utf-8") as f:
             character_text_json = json.load(f)
         # Character status file purely includes the atk/hp curves for characters.
         # The level 10 entry for each character (keyed on their ID) is the "base" atk/hp
         # for that character.
-        with open(f"{data_dir}/character/character_status.json", "r") as f:
+        with open(f"{data_dir}/character/character_status.json", "r", encoding="utf-8") as f:
             character_status_json = json.load(f)
         # Action skills use the "internal name" of a character as the key to access
         # their info.
-        with open(f"{data_dir}/skill/action_skill.json", "r") as f:
+        with open(f"{data_dir}/skill/action_skill.json", "r", encoding="utf-8") as f:
             action_skill_json = json.load(f)
-        with open(f"{data_dir}/ability/ability.json", "r") as f:
+        with open(f"{data_dir}/ability/ability.json", "r", encoding="utf-8") as f:
             abilities_json = json.load(f)
 
         for key in character_json:
