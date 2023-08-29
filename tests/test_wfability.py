2ba4f9ccad3b65c229ba369b538a729dc79d47fa
Vincent <vince.raiken@gmail.com>
2023-08-29 00:50:56 -0400

    More unit tests; Fix float equality check
    Need to be using "almost equals" function
    on TestCase. Otherwise the floating point
    representation with its slight inaccuracies
    could cause a test to fail even if the output
    was actually correct.

--- tests/test_wfability.py
+++ tests/test_wfability.py
@@ -10,6 +10,31 @@ class TestWorldFlipperAbility(TestCase):
     def setUpClass(cls) -> None:
         cls.wf_data = WorldFlipperData("wf_data_json")
 
+    def test_pf_increase(self):
+        """
+        Checks the simple common effect of increasing powerflip damage with no conditions.
+        """
+        vagner, state = self._base_state("fire_dragon")
+        df = vagner.abilities[0][0].eval_effect(vagner, state)
+        self.assertAlmostEqual(0.3, df.stat_mod_pf_damage)
+
+    def test_every_pfs_atk_this_unit(self):
+        """
+        Checks that the condition based on number of powerflips is working along with the effect
+        that increases attack. As an extra check, this is making sure that the "this unit" target
+        is working correctly by both applying the ability to the unit which benefits from the
+        powerflips condition being met, and an unrelated unit.
+        """
+        vagner, state = self._base_state("fire_dragon")
+        ahanabi = self.wf_data.find("kunoichi_1anv")
+        state.set_member(ahanabi, CharPosition.UNISON, 0)
+        state.set_powerflips(0, 15)
+
+        df = vagner.abilities[1][0].eval_effect(vagner, state)
+        self.assertAlmostEqual(0.45, df.attack_modifier)
+        df = vagner.abilities[1][0].eval_effect(ahanabi, state)
+        self.assertIs(None, df)
+
     def test_skill_activated_condition_ahanabi_above_cap(self):
         """
         Ensure that skill activation stacks don't go above the +attack cap for AHanabi's ability 2.
@@ -18,7 +43,7 @@ class TestWorldFlipperAbility(TestCase):
         state.set_skill_activations(0, 20)
 
         df = ahanabi.abilities[1][0].eval_effect(ahanabi, state)
-        self.assertEqual(0.4, df.attack_modifier)
+        self.assertAlmostEqual(0.4, df.attack_modifier)
 
     def test_skill_activated_condition_ahanabi_multiple_fire(self):
         """
@@ -36,10 +61,10 @@ class TestWorldFlipperAbility(TestCase):
         state.set_skill_activations(1, 3)
 
         df = ahanabi.abilities[1][0].eval_effect(ahanabi, state)
-        self.assertEqual(0.16, df.attack_modifier)
+        self.assertAlmostEqual(0.16, df.attack_modifier)
 
         df = ahanabi.abilities[1][0].eval_effect(vagner, state)
-        self.assertEqual(0.24, df.attack_modifier)
+        self.assertAlmostEqual(0.24, df.attack_modifier)
 
     def _base_state(self, char_name: str):
         state = GameState()
